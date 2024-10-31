ecb4b3d90dc13458d94142f6feb9533a2e9c29dd
Eguamanplg <eguaman@plataformagroup.cl>
2024-10-31 02:17:05 -0300

    Creating model and new database for Authenticate

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -7,6 +8,35 @@ namespace NZWalks.API.Data
     {
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base( options )
         {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating( builder );
+
+            var readerRoleId = "00eaec85-cc4e-4e85-ab50-dce6a0c41211";
+            var writerRoleId = "fa1f4d77-cb75-482b-9fd7-504f50690e8a";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp=readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "READER"
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp=writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "WRITER"
+                }
+            };
+
+            builder.Entity<IdentityRole>( ).HasData( roles );
 
         }
     }
